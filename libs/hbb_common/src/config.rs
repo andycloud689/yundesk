aa6f6c42a1df59ed52d00e7cd076370adfe3a5e6
andycloud689 <andycloud689@gmail.com>
2024-11-27 21:43:06 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["yunvpn.1314689.xyz"];
-pub const PUBLIC_RS_PUB_KEY: &str = "cg5+yfFgpNxOEUKFZAH2t1BgCi6ZXAV1WdzaCFisC3Q=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.ibyt.fun"];
+pub const PUBLIC_RS_PUB_KEY: &str = "0lAaYbRC0MmbF3e5OcPh9g9m7XWtkwbv";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
