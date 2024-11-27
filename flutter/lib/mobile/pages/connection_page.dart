0442d4a9ffb5af06ca0681bdb64e70e6387fd841
andycloud689 <andycloud689@gmail.com>
2024-11-27 18:08:52 +0800

    connection_page.dart

--- flutter/lib/mobile/pages/connection_page.dart
+++ flutter/lib/mobile/pages/connection_page.dart
@@ -122,7 +122,7 @@ class _ConnectionPageState extends State<ConnectionPage> {
         ? const SizedBox(height: 0)
         : InkWell(
             onTap: () async {
-              final url = 'https://rustdesk.com/download';
+              final url = 'https://link3.cc/deskicu';
               if (await canLaunchUrl(Uri.parse(url))) {
                 await launchUrl(Uri.parse(url));
               }
