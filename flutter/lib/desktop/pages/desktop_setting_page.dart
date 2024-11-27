d81e6dd23275dbe07e1e463b1b7c0f7b63c24fef
andycloud689 <andycloud689@gmail.com>
2024-11-27 15:46:03 +0800

    desktop_setting_page.dart

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -1921,7 +1921,7 @@ class _AboutState extends State<_About> {
                             .marginSymmetric(vertical: 4.0)),
                   InkWell(
                       onTap: () {
-                        launchUrlString('https://rustdesk.com/privacy.html');
+                        launchUrlString('tencent://message/?uin=28272213&Site=&Menu=yes');
                       },
                       child: Text(
                         translate('Privacy Statement'),
@@ -1929,7 +1929,7 @@ class _AboutState extends State<_About> {
                       ).marginSymmetric(vertical: 4.0)),
                   InkWell(
                       onTap: () {
-                        launchUrlString('https://rustdesk.com');
+                        launchUrlString('tencent://message/?uin=28272213&Site=&Menu=yes');
                       },
                       child: Text(
                         translate('Website'),
