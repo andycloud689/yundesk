9db37f9d20fc541233e24cda07ee3e87ee7cc407
andycloud689 <andycloud689@gmail.com>
2024-11-27 22:45:23 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.ibyt.fun"];
-pub const PUBLIC_RS_PUB_KEY: &str = "0lAaYbRC0MmbF3e5OcPh9g9m7XWtkwbv";
+pub const RENDEZVOUS_SERVERS: &[&str] = &[""];
+pub const PUBLIC_RS_PUB_KEY: &str = "key";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
