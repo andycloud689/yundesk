fcdeec21535e809171fe7b8986c8f763b938dbe0
andycloud689 <andycloud689@gmail.com>
2024-11-27 19:13:50 +0800

    connection_page.dart

--- flutter/lib/mobile/pages/connection_page.dart
+++ flutter/lib/mobile/pages/connection_page.dart
@@ -122,7 +122,7 @@ class _ConnectionPageState extends State<ConnectionPage> {
         ? const SizedBox(height: 0)
         : InkWell(
             onTap: () async {
-              final url = 'https://link3.cc/deskicu';
+              final url = 'https://link3.cc/deskorg';
               if (await canLaunchUrl(Uri.parse(url))) {
                 await launchUrl(Uri.parse(url));
               }
