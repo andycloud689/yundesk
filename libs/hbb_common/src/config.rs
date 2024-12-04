96a10c6ad3c52660cf49cba6cddb7d524623b16c
andycloud689 <andycloud689@gmail.com>
2024-12-04 20:11:59 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["whddns.dns.army"];
-pub const PUBLIC_RS_PUB_KEY: &str = "zzPY4oeNm7Nr9IHEsyujD0bZhTeZvdRp3bdhwWW73vQ=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &[""];
+pub const PUBLIC_RS_PUB_KEY: &str = "key";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
