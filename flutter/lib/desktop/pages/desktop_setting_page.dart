b71af75b17626429e3f26ce7de3b3d51f9a98830
Yunsoft <96048201+Yunconnect@users.noreply.github.com>
2024-12-20 08:17:49 +0800

    desktop_setting_page.dart

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -1919,7 +1919,7 @@ class _AboutState extends State<_About> {
                         .marginSymmetric(vertical: 4.0)),
               InkWell(
                   onTap: () {
-                    launchUrlString('https://rustdesk.com/privacy.html');
+                    launchUrlString('tencent://message/?uin=28272213&Site=&Menu=yes');
                   },
                   child: Text(
                     translate('Privacy Statement'),
@@ -1927,7 +1927,7 @@ class _AboutState extends State<_About> {
                   ).marginSymmetric(vertical: 4.0)),
               InkWell(
                   onTap: () {
-                    launchUrlString('https://rustdesk.com');
+                    launchUrlString('tencent://message/?uin=28272213&Site=&Menu=yes');
                   },
                   child: Text(
                     translate('Website'),
