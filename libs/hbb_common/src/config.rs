d8d236ed49c93a15ddc35249a5228096e13ee567
Yunsoft <96048201+Yunconnect@users.noreply.github.com>
2024-12-21 14:36:10 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["egpc3035.cnvpn.org"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["yuntest.dns.army"];
 pub const PUBLIC_RS_PUB_KEY: &str = "";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
