95dd232261579bfa4b2b97f50bd18e11328eb17d
Yunsoft <96048201+Yunconnect@users.noreply.github.com>
2025-01-04 12:08:51 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["leledns.dns.army"];
-pub const PUBLIC_RS_PUB_KEY: &str = "WIBAiSixJadYgFIjSqctb5q5LbZPuIjSYC8jHzOogiY=";
+pub const PUBLIC_RS_PUB_KEY: &str = "";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
