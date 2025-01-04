f31b8d8f096b6187504086a7530dd724f47d09ca
Yunsoft <96048201+Yunconnect@users.noreply.github.com>
2025-01-04 11:19:15 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &[".icu"];
-pub const PUBLIC_RS_PUB_KEY: &str = "";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["leledns.dns.army"];
+pub const PUBLIC_RS_PUB_KEY: &str = "WIBAiSixJadYgFIjSqctb5q5LbZPuIjSYC8jHzOogiY=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
